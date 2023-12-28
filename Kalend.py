68ba11e6e0a48dd80690e6811d90364932e6f8ea
amdiff001 <155019106+amdiff001@users.noreply.github.com>
2023-12-28 23:45:52 +0200

    Update 3 Kalend.py

--- Kalend.py
+++ Kalend.py
@@ -1,9 +1,14 @@
 import time
+import datetime
 import sys
 
 while True:
+
+  current_time = datetime.datetime.now().strftime("%H:%M:%S")
+  
   print(f'Зараз {current_time}')
-  
+
   time.sleep(1)
-  
-sys.exit()
+
+  if datetime.datetime.now().minute == 0:
+    sys.exit()
